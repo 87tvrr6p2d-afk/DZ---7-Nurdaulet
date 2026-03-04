0ff31a454fc36e5526b1cd0aa596d9a407be90f2
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-03-04 11:41:09 +0500

    Create MainTemplate.java

---
+++ MainTemplate.java
@@ -0,0 +1,12 @@
+public class MainTemplate {
+    public static void main(String[] args) {
+        Beverage tea = new Tea();
+        tea.prepareRecipe();
+
+        Beverage coffee = new Coffee();
+        coffee.prepareRecipe();
+
+        Beverage choco = new HotChocolate();
+        choco.prepareRecipe();
+    }
+}
