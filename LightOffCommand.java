87170ebaec22b3ca44fe3f941c0b6e3eb248817c
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-03-04 11:35:45 +0500

    Create LightOffCommand.java

---
+++ LightOffCommand.java
@@ -0,0 +1,11 @@
+public class LightOffCommand implements ICommand {
+    private final Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override public void execute() { light.off(); }
+    @Override public void undo() { light.on(); }
+    @Override public String name() { return "Light OFF"; }
+}
